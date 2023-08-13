95fd1e33ec0ff41848fd09fc5ba49ac76f9c133a
Tom Daniel <tomudoh258@gmail.com>
2023-08-13 23:15:53 +0100

    Setup Devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Set the root route to the splash page
+  root "pages#splash"
+
+  # Define other application routes as needed
+
 end

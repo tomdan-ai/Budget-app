0294288535b53d867376439797b2ea865c0cba25
Tom Daniel <tomudoh258@gmail.com>
2023-08-18 12:49:36 +0100

    Create methodsand params for controlers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-    # ...
-  
-   
-    private
+  # ...
 
-    def user_params
-      params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
-    end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
   end
-  
+end

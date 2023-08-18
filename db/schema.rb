37ab033935aef9913590cb5c4806fe6a541feb1c
Tom Daniel <tomudoh258@gmail.com>
2023-08-18 12:43:06 +0100

    Complete configurations and migartions in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_181734) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_200940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_181734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "icon"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
